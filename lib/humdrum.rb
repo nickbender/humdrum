28ebd4417fc359fbc9a677ed9aa3bbb5d689dae4
nickbender <nickbender@me.com>
2013-03-25 16:31:47 -0600

    catch non-email input

--- lib/humdrum.rb
+++ lib/humdrum.rb
@@ -3,7 +3,7 @@ require "humdrum/humdrum_emails.rb"
 
 module Humdrum
   def self.is_common?(email)
-  	return false if email.nil?
+  	return false if email.nil? or not email.include?("@")
   	email = email.split("@")[1].downcase.strip
   	HumdrumEmails.list.has_key?(email)
   end
